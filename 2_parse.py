fc5865d03252f7440bc0496f1f58d2ed45dfcf0e
wall-e <zzgigi2003@163.com>
2019-12-09 12:00:22 +0800

    add 1_downloader.py,2_parse.py

---
+++ 2_parse.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# coding:utf-8
+"""
+从Html文件中解析车辆信息
+"""
+
+
+import sys
+import os
+import json
+
+from bs4 import BeautifulSoup
+
+def parse_html(html):
+    """
+    parse html of https://vincar.cn/
+    :param html: html strings
+    :returns: list
+    """
+    soup = BeautifulSoup(html, 'html.parser')
+    for i in soup.find_all('strong'):
+        if i.text.strip() == "市中标公告":
+            return i.parent
+
+
+def main():
+    """
+    main function
+    """
+    if len(sys.argv) < 2:
+        print("%s filename.html" % (sys.argv[0]))
+        sys.exit(1)
+
+    filename = sys.argv[1]
+    if not os.path.exists(filename):
+        print("%s is not exists" % (filename))
+        sys.exit(1)
+    html = open(filename).readlines()
+    data = parse_html("".join(html))
+    print("%s/%s" % ("http://dycg.dongying.gov.cn/sdgp2014/site", data.attrs["href"]))
+
+
+if __name__ == "__main__":
+    main()
